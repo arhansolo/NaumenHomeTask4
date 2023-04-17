bb378196843402978e7851a7f459c39c81451c90
arhansolo <arhansolo@yandex.ru>
2023-04-17 16:45:05 +0500

    completed task2

--- src/main/java/ru/urfu/Main.java
+++ src/main/java/ru/urfu/Main.java
@@ -1,7 +1,23 @@
 package ru.urfu;
 
 public class Main {
-    public static void main(String[] args) {
-        //TODO
+    /**
+     * Переехать из текущего места в заданную точку
+     * на любом, заранее определённом транспорте
+     */
+    public static void moveTo(Person person, Position destination) {
+        //реализация могла быть проще, если бы в качестве аргументов передавался ещё и определённый транспорт. Можно было бы обойти
+        //реализацию методов get/set position у Bike
+        Bike bike = new Bike();
+        if (bike.getPosition() != person.getPosition()) { //проверяем, находится ли человек в транспорте или нет
+            person.walk(bike.getPosition());
+        }
+
+        bike.drive(person, destination);
+        if (person.getPosition() != destination){
+            person.walk(destination);
+        }
+        // TODO
+        assert person.getPosition() == destination;
     }
 }

170c11d167cff2c986a8ecd66d56f919c7af7f94
arhansolo <arhansolo@yandex.ru>
2023-04-17 23:00:04 +0500

    fixed comments

--- src/main/java/ru/urfu/Main.java
+++ src/main/java/ru/urfu/Main.java
@@ -5,13 +5,6 @@ public class Main {
      * Переехать из текущего места в заданную точку
      */
     public static void moveTo(Person person, Position destination) {
-        //закомментировал альтернативный вариант без создания класса определённого авто (т.е. Subaru)
-//        Car Subaru = new Car() {
-//            @Override
-//            public void drive(Person person, Position destination) {
-//                person.setPosition(destination);
-//            }
-//        };
         Subaru subaru = new Subaru();
         subaru.drive(person, destination);
         if (person.getPosition() != destination) { //машина не всегда может довезти человека до нужного места, поэтому сравниваем координаты
